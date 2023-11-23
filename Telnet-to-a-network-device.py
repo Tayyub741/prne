1ae03c15cf028fc0caaa9f10448e2ab588267d13
Tayyub741 <151837662+Tayyub741@users.noreply.github.com>
2023-11-23 12:53:44 +0000

    Update Telnet-to-a-network-device.py

--- Telnet-to-a-network-device.py
+++ Telnet-to-a-network-device.py
@@ -27,75 +27,192 @@ print('----            Username:   ', ip_address)
 print('----            Password:   ', ip_address)
 print('')
 print('------------------------------------------------------')
-
 #Set the terminal length to 0
-    session.sendline('terminal length 0')
-    result= session.expect(['#'])
+   session.sendline('terminal length 0')
+   result= session.expect(['#'])
 
-    #Check the running config
-    session.sendline("show run brief")
-    result= session.expect(['#'])
-    config_info = session.before
+   #Check the running config
+   session.sendline("show run brief")
+   result= session.expect(['#'])
+   config_info = session.before
+
+   #Write into a file the running config
+   outfile = open('config_1.txt', 'w')
+   outfile.write(config_info)
+
+   #Output the running config
+   #print('This is the: ',config_info)
+
+   #Display message that the user has successfully saved the config, if it works.
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('--- You have saved the running config succesfully!')
+   print('')
+   print('-------------------------------------------------------------------------')
+
+   #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('FAILED TO SAVE CONFIG, CONFIG NOT FOUND:')
+       exit()
+
+   #Display message that the user has successfully chnaged the hostname, if it works.
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('--- You have saved the running-config succesfully!: ')
+   print('')
+   print('-------------------------------------------------------------------------')
+
+   #Enter the congiure terminal
+   session.sendline("conf t")
+   result= session.expect(['#'])
+
+   #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('FAILED TO ENTER CONFIGURE TERMINAL , INVALID INPUT:')
+       exit()
+
+   #Display message that the user has successfully entered the host terminal, if it works.
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('--- You have sucessfully entered the configure terminal: ')
+   print('')
+   print('-------------------------------------------------------------------------')
 
 
-#Enable System logs to keep track of all actions
-    command = 'echo logging on'
-    result = subprocess.run(command, shell=True)
+   #Enable secret for accessing enable.
 
-     #Check for any errors if any are discovered show a warning message and exit.
-    if result != 0:
-        print('FAILED TO CHANGE SYSTEM LOGS , INVALID INPUT:')
-        exit()
-
-    #Display message that the user has successfully added Exec timeout, if it works.
-    print('')
-    print('-------------------------------------------------------------------------')
-    print('')
-    print('-------------------------------------------------------------------------')
-    print('Successfully added System logs timeout!')
-    print('')
-    print('-------------------------------------------------------------------------')
+   session.sendline("username prne secret cisco123!")
+   result= session.expect(['#'])
+   print('Successfully enabled secret')
 
 
-#Display message that the user has successfully logged in, if it works.
-    print('-------------------------------------------------------------------------')
-    print('')
-    print('--- You have logged in succesfully! connecting to:', ip_address)
-    print('---                                      Username: ' , username)
-    print('---                                      Password: ' , password)
-    print('')
-    print('-------------------------------------------------------------------------')
+   #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('FAILED TO password , INVALID INPUT:')
+       exit()
 
-    #Set the terminal length to 0
-    session.sendline('terminal length 0')
-    result= session.expect(['#'])
+   #Display message that the user has successfully changed the password, if it works.
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('--- You have sucessfully changed the password: ')
+   print('')
+   print('-------------------------------------------------------------------------')
 
-    #Check the running config
-    session.sendline("show startup-config")
-    result= session.expect(['#'])
-    config_info = session.before
+   # Enter line con 0
+   session.sendline("line con 0")
+   result= session.expect(['#'])
 
-    #Write into a file the running config
-    outfile = open('config_2.txt', 'w')
-    outfile.write(config_info)
+   #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('FAILED TO enter line con 0 , INVALID INPUT:')
+       exit()
+
+   #Display message that the user has successfully entered line con 0, if it works.
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('Successfully entered line con 0')  
+   print('')
+   print('-------------------------------------------------------------------------')
+
+   # Enter Exec timeout
+   session.sendline("exec-timeout 10 5")
+   result= session.expect(['#'])
+
+   #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('FAILED TO CHANGE EXEC TIMEOUT , INVALID INPUT:')
+       exit()
+
+   #Display message that the user has successfully added Exec timeout, if it works.
+   print('')
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('-------------------------------------------------------------------------')
+   print('Successfully added Exec timeout!')
+   print('')
+   print('-------------------------------------------------------------------------')
+  
+   #Enable System logs to keep track of all actions
+   command = 'echo logging on'
+   result = subprocess.run(command, shell=True)
+
+    #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('FAILED TO CHANGE SYSTEM LOGS , INVALID INPUT:')
+       exit()
+
+   #Display message that the user has successfully added Exec timeout, if it works.
+   print('')
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('-------------------------------------------------------------------------')
+   print('Successfully added System logs timeout!')
+   print('')
+   print('-------------------------------------------------------------------------')
+
+   #Change the configuration to CONFIG3
+   session.sendline("no service password-recovery")
+   result= session.expect(['#'])
+
+   #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('FAILED TO CHANGE NO PASSWORD RECOVERY , INVALID INPUT:')
+       exit()
+
+   #Display message that the user has successfully changed config, if it works.
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('Successfully added no password recovery')  
+   print('')
+   print('-------------------------------------------------------------------------')
+
+   #The session needs the password now
+   session.sendline(password)
+   result= session.expect(['#', pexpect.TIMEOUT])
+
+   #Check for any errors if any are discovered show a warning message and exit.
+   if result != 0:
+       print('SESSION LOGIN FAILED PASSWORD DOES NOT MATCH USERNAME:', password)
+       exit()
+
+   #Display message that the user has successfully logged in, if it works.
+   print('-------------------------------------------------------------------------')
+   print('')
+   print('--- You have logged in succesfully! connecting to:', ip_address)
+   print('---                                      Username: ' , username)
+   print('---                                      Password: ' , password)
+   print('')
+   print('-------------------------------------------------------------------------')
+
+   #Set the terminal length to 0
+   session.sendline('terminal length 0')
+   result= session.expect(['#'])
+
+   #Check the running config
+   session.sendline("show startup-config")
+   result= session.expect(['#'])
+   config_info = session.before
+
+   #Write into a file the running config
+   outfile = open('config_2.txt', 'w')
+   outfile.write(config_info)
 
 
-    file_path_1 = "/home/devasc/labs/prne/config_1.txt"
+   file_path_1 = "/home/devasc/labs/prne/config_1.txt"
 
-    file_path_2 = "/home/devasc/labs/prne/config_2.txt"
-
-   
-    command = f"diff {file_path_1} {file_path_2}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
-
-    if result.returncode == 0:
-        print("Files are identical.")
-    else:
-        print("Files differ:")
-        print(result.stdout)
+   file_path_2 = "/home/devasc/labs/prne/config_2.txt"
 
   
+   command = f"diff {file_path_1} {file_path_2}"
+   result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
-#Terminate telnet to device and close session
-session.sendline('quit')
-session.close
+   if result.returncode == 0:
+       print("Files are identical.")
+   else:
+       print("Files differ:")
+       print(result.stdout)
+
+   #Terminate telnet to device and close the session
+   session.sendline('quit')
+   session.close()
+   exit()    
